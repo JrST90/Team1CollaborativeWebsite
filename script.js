d11392cb435e18aafcacfe4957866e6ea3a95031
JrST90 <55492721+JrST90@users.noreply.github.com>
2025-09-11 11:33:52 -0400

    Add files via upload
    Initial files for Team1CollaborativeWebsite, basic HTML, CSS, and JavaScript files.

---
+++ script.js
@@ -0,0 +1,5 @@
+/*
+To Do: Grab the values from the input boxes in the div labeled "one" which is labeled as "JavaScript Functions" on the webpage,
+store them in an array in the JavaScript code titled script.js, and out put them into the div labeled "two" which is labeled as
+"JavaScript Results" on the webpage
+*/
