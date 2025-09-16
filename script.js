8dc9d639d70ce5a27bd54b32b271afdc871c0145
Elian Garcia <gar5710@pennwest.edu>
2025-09-16 13:02:33 -0400

    Added the submit button to the html and added functionality to the script.js file.

--- script.js
+++ script.js
@@ -3,3 +3,33 @@ To Do: Grab the values from the input boxes in the div labeled "one" which is la
 store them in an array in the JavaScript code titled script.js, and out put them into the div labeled "two" which is labeled as
 "JavaScript Results" on the webpage
 */
+
+function displayInfo() {
+    
+	// Grabs all inputs with name="userID"
+    
+	let inputs = document.getElementsByName("userID");
+
+    // Turn the NodeList into array of values
+    
+	let infoArray = [];
+    
+	for (let i = 0; i < inputs.length; i++) {
+    
+		infoArray.push(inputs[i].value);
+    
+	}
+
+    // Output format
+	
+    let output = `
+        
+		<p><strong>Name:</strong> ${infoArray[0]}</p>
+        <p><strong>Major:</strong> ${infoArray[1]}</p>
+        <p><strong>Favorite Course:</strong> ${infoArray[2]}</p>
+        <p><strong>Graduation Date:</strong> ${infoArray[3]}</p>`;
+
+    // Display 
+	
+    document.getElementById("two").innerHTML = output;
+}
